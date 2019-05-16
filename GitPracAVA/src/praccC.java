5b5077abe0744892977d267824d39a34017bf430
Srinitya Allam <AllamS20@pwcs-edu.org>
2019-05-16 13:56:02 -0400

    File Updated

--- GitPracAVA/src/praccC.java
+++ GitPracAVA/src/praccC.java
@@ -6,9 +6,9 @@ public class praccC {
 		System.out.println("Give me your favorite smallest integer?");
 		Scanner in = new Scanner(System.in);
 		int x = in.nextInt();
+		System.out.println("Give me your favorite largest integer?"); // This line was switched with the line below
 		int y = in.nextInt();
-		System.out.println("Give me your favorite largest integer?");
-		System.out.println("Now selecting a random number from " + x + " to " + y + " : " + (nextInt(x-y) + y));
+		System.out.println("Now selecting a random number from " + x + " to " + y + " : " + (m.nextInt(y-x) + x)); // needed m.nextInt(), y and x, y is switched with x
 		
 		
 
